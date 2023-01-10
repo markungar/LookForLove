e2f339619005dadba0ea34ea6d9a70c450e9b2fe
markungar <121110471+markungar@users.noreply.github.com>
2023-01-10 09:10:00 -0500

    Update Prompts.java

--- Prompts.java
+++ Prompts.java
@@ -4,12 +4,17 @@ public class Prompts {
     protected String favMovie;
     protected String favSeason;
     protected String favFood;
-    protected String favArtist;
+    protected String favGenre;
+    protected String[] prompts = new String[4]; 
 
     public Prompts (String favMovie, String favSeason, String favFood, String favArtist) {
         this.favMovie = favMovie;
         this.favSeason = favSeason;
         this.favFood = favFood;
         this.favArtist = favArtist;
+    }
+    
+    public void createPrompt(int index, String prompt) {
+        prompts[index] = prompt;
     }
 }
