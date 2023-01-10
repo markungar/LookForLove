0aad7bbb86d07a01e1bdf213f281ca0de7c749b9
markungar <121110471+markungar@users.noreply.github.com>
2023-01-10 14:33:53 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,23 +1,31 @@
-package LookForLove;
-
 public abstract class Person {
     protected Account loginInfo;
     protected Character trait;
     protected WishCharacter wish;
     protected String description;
+    //declares all characteristics of a person, including their account details, trait details, ideal partner details and a brief description of themselves 
+    
     protected String[] prompts = new String[4];
+    //declares prompt array
 
-    public Person (Account loginInfo, Character trait, WishCharacter wish, String description, String[] prompts) {
+    public Person (Account loginInfo, Character trait, WishCharacter wish, String description, String favMovie, String favSport, String favSeason, String favGenre) {
         this.loginInfo = loginInfo;
         this.trait = trait;
         this.wish = wish;
         this.description = description;
-        this.prompts = prompts;
+        
+        prompts[0] = favMovie;
+        prompts[1] = favSport;
+        prompts[2] = favSeason;
+        prompts[3] = favGenre;
+        //sets the persons prompt answers in correct order
     }
+    //constructor for person, initilizing all fields
 
     public String toString() {
-        return loginInfo.toString() + "\n" + trait + "\n" + wish + "\n" + prompts;
+        return loginInfo.toString() + "\n" + trait + "\n" + wish + "\ndescription: " + description + "\nfavMovie: " + prompts[0];
     }
+    //to string for trait, wish, description and prompts of specified person
 
     public Account getLoginInfo() {
         return loginInfo;
@@ -38,6 +46,8 @@ public abstract class Person {
     public String[] getPrompts() {
         return prompts;
     }
+    
+    //ALL GET METHODS
 
     public Account setLoginInfo(Account loginInfo) {
         this.loginInfo = loginInfo;
@@ -59,6 +69,8 @@ public abstract class Person {
         return description;
     }
 
+    //ALL SET METHODS
+    
     public boolean setPrompts(String favMovie, String favSport, String favSeason, String favGenre) {
         try
         {
@@ -75,10 +87,12 @@ public abstract class Person {
         
         return true;
     }
+    //sets/changes someones prompt answers without use of the constructor 
 
     public int compareTo(Person other) {
         return this.trait.age - other.trait.age;
     }
+    //compares this persons age to another persons age, returning the age differential
     
     public boolean equals(Person other) {
         if (this.loginInfo.getPassword() == other.loginInfo.getPassword() && this.loginInfo.getUsername().equals(other.loginInfo.getUsername())) {
@@ -87,10 +101,17 @@ public abstract class Person {
             return false;
         }
     }
+    //checks if the certain account has the same username as someone elses, same with password
     
     public abstract int totalScore(Person other);
+    //calculates the total score of prompts and wishes added together
+    
     public abstract int promptsScore(Person other);
+    //calculates the matching prompts between two people, and displays a score of points that change due to the gender of the person
+    
     public abstract int wishScore(Person other);
+    //calculates the wish score (age range etc.) of two people, and displays a score of points that change due to the gender of the person
+    
 
 
 }
