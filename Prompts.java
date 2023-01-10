d4fb259ebd70ad34cfe276dd12dec2926a662302
markungar <121110471+markungar@users.noreply.github.com>
2023-01-10 14:06:57 -0500

    Update Prompts.java

--- Prompts.java
+++ Prompts.java
@@ -3,15 +3,20 @@ package LookForLove;
 public class Prompts {
     protected String favMovie;
     protected String favSeason;
-    protected String favFood;
+    protected String favSport;
     protected String favGenre;
     protected String[] prompts = new String[4]; 
 
-    public Prompts (String favMovie, String favSeason, String favFood, String favArtist) {
+    public Prompts (String favMovie, String favSeason, String favSport, String favGenre) {
         this.favMovie = favMovie;
         this.favSeason = favSeason;
         this.favFood = favFood;
-        this.favArtist = favArtist;
+        this.favGenre = favGenre;
+        
+        prompts[0] = favMovie;
+        prompts[1] = favSeason;
+        prompts[2] = favSport;
+        prompts[3] = favGenre;
     }
     
     public void createPrompt(int index, String prompt) {
