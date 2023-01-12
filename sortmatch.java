5001e2bbabebabeb413e5a5f79c5071f12eda925
d22046 <82061238+d22046@users.noreply.github.com>
2023-01-12 15:11:18 -0500

    Add files via upload

---
+++ sortmatch.java
@@ -0,0 +1,13 @@
+
+
+	public PersonScorePair[] sortMatches(LinkedList<PersonScorePair> list) {
+
+		PersonScorePair[] people = list.toArray();
+		for (int i = 0; i < people.size(); i++) {
+			
+			
+
+		}
+		
+
+	}
